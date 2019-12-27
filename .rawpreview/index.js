a79e6d7cb4cc615a599ad2e9528ec7de4a5b5b66
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2019-12-27 09:54:35 +0000

    Regular update: 2019/12/27 09:54 UTC

---
+++ .rawpreview/index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+
+const exec = require('child_process').exec;
+
+
+
+exec('mkdir doc; mkdir doc/.rawpreview; mkdir doc/.rawpreview/config;');
+exec('mkdir doc/.rawpreview/html;');
+exec('touch doc/.rawpreview/config/list-of-files.txt;');
+exec('touch doc/.rawpreview/config/github-username.txt;');
+exec('touch doc/.rawpreview/config/github-repo.txt;');
+
+//
+// ---------------------------------------------------------------
+// Read config
+// ---------------------------------------------------------------
+//
+
+const config = {
+    username: fs.readFileSync('docs/.rawpreview/config/github-username.txt').toString().trim(),
+    repo: fs.readFileSync('docs/.rawpreview/config/github-repo.txt').toString().trim(),
+    files: fs.readFileSync('docs/.rawpreview/config/list-of-files.txt').toString().trim().split('\n').join('[[4f4c84781da6]]')
+}
+
+//
+// ---------------------------------------------------------------
+// Functions
+// ---------------------------------------------------------------
+//
+
+const builder = function () {
+    const indexPageTemplateDefault = fs.readFileSync(__dirname + '/base-template.html').toString().trim().replace('{{LIST}}', config.files).replace('{{USERNAME}}', config.username).replace('{{REPO}}', config.repo);
+    fs.writeFileSync('docs/.rawpreview/index.html', indexPageTemplateDefault);
+};
+
+//
+// ---------------------------------------------------------------
+// Command implementations
+// ---------------------------------------------------------------
+//
+
+
+builder();
+console.log('Build completed.');
