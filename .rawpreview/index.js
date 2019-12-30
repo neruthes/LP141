4c3a1fdbc8e462ea51b3a4c1828f2424f60439b9
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2019-12-30 06:27:18 +0000

    Regular update: 2019/12/30 06:27 UTC

--- .rawpreview/index.js
+++ .rawpreview/index.js
@@ -8,18 +8,18 @@ const exec = require('child_process').exec;
 
 
 
-exec('mkdir _; mkdir _/_rawpreview; mkdir _/_rawpreview/config;');
-exec('touch _/_rawpreview/config/list-of-files.txt;');
-exec('touch _/_rawpreview/config/github-username.txt;');
-exec('touch _/_rawpreview/config/github-repo.txt;');
+exec('mkdir _; mkdir _/_.neruthes.rawpreview; mkdir _/_.neruthes.rawpreview/config;');
+exec('touch _/_.neruthes.rawpreview/config/list-of-files.txt;');
+exec('touch _/_.neruthes.rawpreview/config/github-username.txt;');
+exec('touch _/_.neruthes.rawpreview/config/github-repo.txt;');
 
 
 
 
 let config = {
-    username: fs.readFileSync('_/_rawpreview/config/github-username.txt').toString().trim(),
-    repo: fs.readFileSync('_/_rawpreview/config/github-repo.txt').toString().trim(),
-    files: fs.readFileSync('_/_rawpreview/config/list-of-files.txt').toString().trim().split('\n').join('[[4f4c84781da6]]'),
+    username: fs.readFileSync('_/_.neruthes.rawpreview/config/github-username.txt').toString().trim(),
+    repo: fs.readFileSync('_/_.neruthes.rawpreview/config/github-repo.txt').toString().trim(),
+    files: fs.readFileSync('_/_.neruthes.rawpreview/config/list-of-files.txt').toString().trim().split('\n').join('[[4f4c84781da6]]'),
     lastCommit: fs.readFileSync('.git/refs/heads/master').toString().trim()
 };
 
